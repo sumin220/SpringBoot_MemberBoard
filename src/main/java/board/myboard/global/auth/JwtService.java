938fee4388ebd7f1ee366990f2efe0086cc5d9bc
sonsumin <sonsumin00@gmail.com>
2024-09-02 17:55:13 +0900

    Feat: JwtService 인터페이스 작성

---
+++ src/main/java/board/myboard/global/auth/JwtService.java
@@ -0,0 +1,30 @@
+package board.myboard.global.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface JwtService {
+
+    String createAccessToken(String username);
+
+    String createRefreshToken();
+
+    void updateRefreshToken(String username, String refreshToken);
+
+    void destroyRefreshToken(String username);
+
+    void sendToken(HttpServletResponse response, String accessToken, String refreshToken);
+
+    String extractAccessToken(HttpServletRequest request) throws IOException, ServletException;
+
+    String extractRefreshToken(HttpServletRequest request) throws IOException, ServletException;
+
+    String extractUsername(String accessToken);
+
+    void setAccessTokenHeader(HttpServletResponse response, String accessToken);
+    void setRefreshTokenHeader(HttpServletResponse response, String refreshToken);
+
+}

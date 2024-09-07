39017a4f29adc9c9bd5acc1583603234fb1fbbb4
sonsumin <sonsumin00@gmail.com>
2024-09-07 23:06:14 +0900

    Feat: 댓글 엔티티작성

---
+++ src/main/java/board/myboard/domain/comment/entity/Comment.java
@@ -0,0 +1,90 @@
+package board.myboard.domain.comment.entity;
+
+import board.myboard.domain.member.entity.Member;
+import board.myboard.domain.post.entity.Post;
+import board.myboard.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.GenerationType.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "COMMENT")
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "writer_id")
+    private Member writer;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    private Boolean isRemoved = false;
+
+    //== 부모 댓글을 삭제해도 자식 댓글은 남아있음 ==//
+    @OneToMany(mappedBy = "parent")
+    private List<Comment> childList = new ArrayList<>();
+
+
+    //== 연관관계 편의 메서드 ==//
+    public void confirmWriter(Member writer) {
+        this.writer = writer;
+        writer.addComment(this);
+    }
+
+    public void confirmPost(Post post) {
+        this.post = post;
+        post.addComment(this);
+    }
+
+    public void confirmParent(Comment parent) {
+        this.parent = parent;
+        parent.addChild(this);
+    }
+
+    public void addChild(Comment child) {
+        childList.add(child);
+    }
+
+    //== 수정 ==//
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    //== 삭제 ==//
+    public void remove() {
+        this.isRemoved = true;
+    }
+
+    @Builder
+    public Comment(Member writer, Post post, Comment parent, String content) {
+        this.writer = writer;
+        this.post = post;
+        this.parent = parent;
+        this.content = content;
+        this.isRemoved = false;
+    }
+}

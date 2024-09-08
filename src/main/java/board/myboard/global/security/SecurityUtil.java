70f1f9c78846dc65decf80ce2a178c01b11eeb45
sonsumin <sonsumin00@gmail.com>
2024-09-08 14:55:08 +0900

    Feat: 인증된 사용자 가져오기

---
+++ src/main/java/board/myboard/global/security/SecurityUtil.java
@@ -0,0 +1,13 @@
+package board.myboard.global.security;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtil {
+
+    public static String getLoginUsername() {
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return user.getUsername();
+    }
+}

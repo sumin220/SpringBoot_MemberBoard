29e1318b418cceee6cfd16e6cabceb0c7852dfa8
sonsumin <sonsumin00@gmail.com>
2024-09-08 21:16:59 +0900

    [#9]Feat: 멤버 Exception

--- src/main/java/board/myboard/domain/member/exception/MemberException.java
+++ src/main/java/board/myboard/domain/member/exception/MemberException.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class MemberException {
+public class MemberException extends RuntimeException {
 
     private final ErrorCode errorCode;
 }

d0c3b379fb8411f9aee17211292e6b19228b542d
sonsumin <sonsumin00@gmail.com>
2024-09-08 20:19:26 +0900

    [#5]Refactor: 비밀번호 암호화

--- src/main/java/board/myboard/global/config/SecurityConfig.java
+++ src/main/java/board/myboard/global/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
 

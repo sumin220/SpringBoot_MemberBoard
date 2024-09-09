d9f5d5407123e70184878e0afb588c01109b8f57
sonsumin <sonsumin00@gmail.com>
2024-09-09 18:27:38 +0900

    [#11]Feat: 댓글 엔티티 구현

--- src/main/java/board/myboard/domain/comment/entity/Comment.java
+++ src/main/java/board/myboard/domain/comment/entity/Comment.java
@@ -11,24 +11,25 @@ import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static jakarta.persistence.FetchType.*;
-import static jakarta.persistence.GenerationType.*;
+import static jakarta.persistence.FetchType.LAZY;
 
-@Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="COMMENT")
 @Getter
-@Table(name = "COMMENT")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Comment extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @Id @GeneratedValue
     @Column(name = "comment_id")
     private Long id;
+
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "writer_id")
     private Member writer;
+
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "post_id")
@@ -42,11 +43,14 @@ public class Comment extends BaseTimeEntity {
     @Column(nullable = false)
     private String content;
 
-    private Boolean isRemoved = false;
+    private boolean isRemoved= false;
+
 
     //== 부모 댓글을 삭제해도 자식 댓글은 남아있음 ==//
     @OneToMany(mappedBy = "parent")
     private List<Comment> childList = new ArrayList<>();
+
+
 
 
     //== 연관관계 편의 메서드 ==//
@@ -60,31 +64,73 @@ public class Comment extends BaseTimeEntity {
         post.addComment(this);
     }
 
-    public void confirmParent(Comment parent) {
+    public void confirmParent(Comment parent){
         this.parent = parent;
         parent.addChild(this);
     }
 
-    public void addChild(Comment child) {
+    public void addChild(Comment child){
         childList.add(child);
     }
+
+
+
 
     //== 수정 ==//
     public void updateContent(String content) {
         this.content = content;
     }
-
     //== 삭제 ==//
     public void remove() {
         this.isRemoved = true;
     }
 
+
     @Builder
-    public Comment(Member writer, Post post, Comment parent, String content) {
+    public Comment( Member writer, Post post, Comment parent, String content) {
         this.writer = writer;
         this.post = post;
         this.parent = parent;
         this.content = content;
         this.isRemoved = false;
+    }
+
+
+    //== 비즈니스 로직 ==//
+    public List<Comment> findRemovableList() {
+
+        List<Comment> result = new ArrayList<>();
+
+        Optional.ofNullable(this.parent).ifPresentOrElse(
+
+                parentComment ->{
+                    //대댓글인 경우 (부모가 존재하는 경우)
+                    if( parentComment.isRemoved()&& parentComment.isAllChildRemoved()){
+                        result.addAll(parentComment.getChildList());
+                        result.add(parentComment);
+                    }
+                },
+
+                () -> {
+                    //댓글인 경우
+                    if (isAllChildRemoved()) {
+                        result.add(this);
+                        result.addAll(this.getChildList());
+                    }
+                }
+        );
+
+        return result;
+    }
+
+
+    //모든 자식 댓글이 삭제되었는지 판단
+    private boolean isAllChildRemoved() {
+        return getChildList().stream()
+                .map(Comment::isRemoved)//지워졌는지 여부로 바꾼다
+                .filter(isRemove -> !isRemove)//지워졌으면 true, 안지워졌으면 false이다. 따라서 filter에 걸러지는 것은 false인 녀석들이고, 있다면 false를 없다면 orElse를 통해 true를 반환한다.
+                .findAny()//지워지지 않은게 하나라도 있다면 false를 반환
+                .orElse(true);//모두 지워졌다면 true를 반환
+
     }
 }

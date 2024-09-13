9aa5f8e7acdb5131b1b441a061ad39967cad8383
sonsumin <sonsumin00@gmail.com>
2024-09-13 20:03:03 +0900

    [#30]Feat: 댓글 비지니스 로직 추가

--- src/main/java/board/myboard/domain/comment/service/CommentService.java
+++ src/main/java/board/myboard/domain/comment/service/CommentService.java
@@ -1,17 +1,16 @@
 package board.myboard.domain.comment.service;
 
-import board.myboard.domain.comment.entity.Comment;
+import board.myboard.domain.comment.dto.CommentSaveDTO;
+import board.myboard.domain.comment.dto.CommentUpdateDTO;
 import board.myboard.domain.comment.exception.CommentException;
-
-import java.util.List;
 
 public interface CommentService {
 
-    void save(Comment comment);
+    void save(Long postId, CommentSaveDTO commentSaveDTO);
 
-    Comment findById(Long id) throws CommentException;
+    void saveReComment(Long postId, Long parentId, CommentSaveDTO commentSaveDTO);
 
-    List<Comment> findAll();
+    void update(Long id, CommentUpdateDTO commentUpdateDTO);
 
     void remove(Long id) throws CommentException;
 }
--- src/main/java/board/myboard/domain/comment/service/impl/CommentServiceImpl.java
+++ src/main/java/board/myboard/domain/comment/service/impl/CommentServiceImpl.java
@@ -1,10 +1,17 @@
 package board.myboard.domain.comment.service.impl;
 
+import board.myboard.domain.comment.dto.CommentSaveDTO;
+import board.myboard.domain.comment.dto.CommentUpdateDTO;
 import board.myboard.domain.comment.entity.Comment;
 import board.myboard.domain.comment.exception.CommentException;
 import board.myboard.domain.comment.repository.CommentRepository;
 import board.myboard.domain.comment.service.CommentService;
+import board.myboard.domain.member.exception.MemberException;
+import board.myboard.domain.member.repository.MemberRepository;
+import board.myboard.domain.post.exception.PostException;
+import board.myboard.domain.post.repository.PostRepository;
 import board.myboard.global.exception.ErrorCode;
+import board.myboard.global.security.SecurityUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,22 +24,53 @@ import java.util.List;
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
 
     @Override
-    public void save(Comment comment) {
+    public void save(Long postId, CommentSaveDTO commentSaveDTO) {
+
+        Comment comment = commentSaveDTO.toEntity();
+
+        comment.confirmWriter(memberRepository.findByUsername(SecurityUtil.getLoginUsername())
+                .orElseThrow(
+                        () -> new MemberException(ErrorCode.NOT_FOUND_MEMBER)
+                ));
+
+        comment.confirmPost(postRepository.findById(postId)
+                .orElseThrow(
+                        () -> new PostException(ErrorCode.NOT_FOUND_BOARD)
+                ));
+
+        commentRepository.save(comment);
+    }
+
+
+    @Override
+    public void saveReComment(Long postId, Long parentId, CommentSaveDTO commentSaveDTO) {
+        Comment comment = commentSaveDTO.toEntity();
+
+        comment.confirmWriter(memberRepository.findByUsername(SecurityUtil.getLoginUsername())
+                .orElseThrow(() -> new MemberException(ErrorCode.NOT_FOUND_MEMBER)));
+
+        comment.confirmPost(postRepository.findById(postId)
+                .orElseThrow(() -> new PostException(ErrorCode.NOT_FOUND_BOARD)));
+
+        comment.confirmParent(commentRepository.findById(parentId)
+                .orElseThrow(() -> new PostException(ErrorCode.NOT_EXIST_COMMENT)));
 
         commentRepository.save(comment);
     }
 
     @Override
-    public Comment findById(Long id) throws CommentException {
-        return commentRepository.findById(id).orElseThrow(
-                () -> new CommentException(ErrorCode.NOT_EXIST_COMMENT));
-    }
+    public void update(Long id, CommentUpdateDTO commentUpdateDTO) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new CommentException(ErrorCode.NOT_EXIST_COMMENT));
 
-    @Override
-    public List<Comment> findAll() {
-        return commentRepository.findAll();
+        if (!comment.getWriter().getUsername().equals(SecurityUtil.getLoginUsername())) {
+            throw new CommentException(ErrorCode.NOT_AUTHORITY_UPDATE_COMMENT);
+        }
+        commentUpdateDTO.content().ifPresent(comment::updateContent);
     }
 
     @Override
@@ -40,8 +78,12 @@ public class CommentServiceImpl implements CommentService {
         Comment comment = commentRepository.findById(id).orElseThrow(
                 () -> new CommentException(ErrorCode.NOT_EXIST_COMMENT));
 
+        if (!comment.getWriter().getUsername().equals(SecurityUtil.getLoginUsername())) {
+            throw new CommentException(ErrorCode.NOT_AUTHORITY_UPDATE_COMMENT);
+        }
+
         comment.remove();
         List<Comment> removableCommentList = comment.findRemovableList();
-        removableCommentList.forEach(removableComment -> commentRepository.delete(removableComment));
+        commentRepository.deleteAll(removableCommentList);
     }
 }
